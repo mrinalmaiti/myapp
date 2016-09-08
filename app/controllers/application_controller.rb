d7afe2d273b0522e269ffb81eb84720f7a3b15a7
mrinalmaiti <mrinal04maiti@gmail.com>
2016-09-08 13:58:51 +0530

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: " This is My Page"
+    render html: " This is My Page from local"
   end
 end
