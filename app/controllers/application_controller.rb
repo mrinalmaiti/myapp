52fab5f4ed7fde69d5a9d8f80a0e3472567b3bbb
mrinalmaiti <mrinal04maiti@gmail.com>
2016-09-08 14:57:54 +0530

    askldf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
+  def hello
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html>
+    render html: " This is My Page from local"
 
   end
 end
